cab073c39c0b0cd7e42b3adf7208a3d6a5eb874e
aliaksandr <koderblokami@gmail.com>
2016-10-15 18:17:46 +0300

    DataBaseUtils.js

---
+++ notes-app/server/utils/DataBaseUtils.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+import Note from '../models/Note';
+
+const Note = mongoose.model('Note');
+
+export function setUpConnection() {
+    mongoose.connect('mongodb://localhost/notes');
+}
+
+export fnction listNotes() {
+    return Note.find();
+}
+
+export function createNote(data) {
+    const note = new Note({
+        title: data.title,
+        text: data.text,
+        color: data.color,
+        createAt: new Date()
+    });
+
+    return note.save();
+}
+
+export function deleteNote(id) {
+    return Note.findId(id).remove();
+}
