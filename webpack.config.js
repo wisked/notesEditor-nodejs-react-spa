9d22b477be6f9c35a3c17c71629bc54f83372291
aliaksandr <koderblokami@gmail.com>
2016-10-15 20:51:16 +0300

    webpack.config.js modified

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,30 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel',
                 exclude: [/node_modules/, /src/]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader",
+                exclude: [/node_modules/, /src/]
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!autoprefixer-loader!less",
+                exclude: [/node_modules/, /src/]
+            },
+            {
+                test: /\.gif$/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
             }
+
         ]
     }
 }
