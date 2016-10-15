398439e84e7ded76e77d0228a79bea6fb9ac524d
aliaksandr <koderblokami@gmail.com>
2016-10-15 18:00:27 +0300

    added mongoDB Schema

--- notes-app/server/app.js
+++ notes-app/server/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send();
+});
 const server = app.listen(8080, () => {
     console.log('server is running');
 });
