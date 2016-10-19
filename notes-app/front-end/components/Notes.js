5c4d44d7cab2e2cd4d7a13d20a5876f6adec0c0a
aliaksandr <koderblokami@gmail.com>
2016-10-19 17:59:16 +0300

    Note component

---
+++ notes-app/front-end/components/Notes.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+
+const Note = React.createClass({
+    render () {
+        return (
+            <h1>
+                Note
+            </h1>
+        )
+    }
+})
+
+export default Note;
