3654d722fc5ec51bf1a155128a03decae0841add
aliaksandr <koderblokami@gmail.com>
2016-10-19 17:21:52 +0300

    added cors midleware

--- notes-app/server/app.js
+++ notes-app/server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { serverPort } from '../etc/config.json';
 
@@ -10,6 +11,8 @@ db.setUpConnection();
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use( cors({origin: '*'}) );
 
 app.get('/notes', (req, res) => {
     db.listNotes().then(data => res.send(data));
